0044b8bf0208a0383e1bba5d3269e370cb50844f
SanchaiPengMos <sanchaipangboot@gmail.com>
2021-05-14 16:45:43 +0700

    add

--- func/api/server.go
+++ func/api/server.go
@@ -1,16 +1,35 @@
 package api
 
 import (
-	configDB "golangdemo/func/config"
-	"golangdemo/func/routes"
+	"database/sql"
+	"golangdemo/func/route"
+	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
+
+// DB
+type UserHandler struct {
+	DB *sql.DB
+}
+
+func (h *UserHandler) Initialize() {
+	// connStr := "postgres://postgres:sanchai@34.87.1.245/demolab?sslmode=disable"
+	conStr2 := "postgres://sanchaipengboot:sanchai@localhost/sanchaipengboot?sslmode=disable"
+	db, err := sql.Open("postgres", conStr2)
+	if err != nil {
+		log.Println(err)
+	}
+	h.DB = db
+
+}
 
 func New() {
 
-	h := configDB.UserHandler{}
+	h := UserHandler{}
 
 	h.Initialize()
 
@@ -20,8 +39,29 @@ func New() {
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
 	}))
 
-	e.GET("/item", routes.GetName)
-	e.GET("/jsonitem", routes.GetjsonName)
+	e.GET("/user", h.GetName)
 
 	e.Logger.Fatal(e.Start(":1234"))
+}
+
+func (h *UserHandler) GetName(c echo.Context) (err error) {
+	rows, err := h.DB.Query("SELECT id , email,firstname,lastname,tel,birthday FROM userhotel order by id asc")
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer rows.Close()
+
+	var CheckUse []route.UserCheck
+
+	for rows.Next() {
+		var data route.UserCheck
+		err := rows.Scan(&data.Id, &data.Email, &data.Firstname, &data.Lastname, &data.Tel, &data.Birthday)
+		if err != nil {
+			log.Println("Scan failed:", err.Error())
+		}
+		CheckUse = append(CheckUse, data)
+	}
+
+	return c.JSON(http.StatusOK, CheckUse)
 }
