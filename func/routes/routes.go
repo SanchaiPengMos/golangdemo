b71d7d8045748754002f8744f9dd40a22c24b183
Sanchai <sanchaipeng@gmail.com>
2021-05-14 11:51:20 +0700

    Create go

---
+++ func/routes/routes.go
@@ -0,0 +1,7 @@
+package routes
+
+import "fmt"
+
+func Run() {
+	fmt.Println("L o a d i n g . . .")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "golangdemo/func/routes"
+
+func main() {
+	routes.Run()
+}
